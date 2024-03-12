b8807351aa33c0196f0b5567a9d12d808fd900ec
sejal99 <sejal8974@gmail.com>
2024-03-12 20:02:08 +0530

    database setup

--- server/db.sql
+++ server/db.sql
@@ -4,3 +4,11 @@ CREATE TABLE restaurants(
     location VARCHAR(50),
     price_range INT
 );
+-- making surethatv all entries are imp to enter
+CREATE TABLE restaurants(
+    id BIGSERIAL NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    price_range INT NOT NULL
+);
+
