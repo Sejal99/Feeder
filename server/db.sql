c7b3b15203f69e592bdfa65a3259892658f68bcb
sejal99 <sejal8974@gmail.com>
2024-03-13 21:39:16 +0530

    database setup

--- server/db.sql
+++ server/db.sql
@@ -1,20 +1,20 @@
-CREATE TABLE restaurants(
-    id INT,
-    name VARCHAR(50),
-    location VARCHAR(50),
-    price_range INT
-);
--- making surethatv all entries are imp to enter
-CREATE TABLE restaurants(
-    id BIGSERIAL NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    location VARCHAR(50) NOT NULL,
-    price_range INT NOT NULL
-);
--- making id as primary key
-CREATE TABLE restaurants(
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    location VARCHAR(50) NOT NULL,
-    price_range INT NOT NULL
-);
+-- CREATE TABLE restaurants(
+--     id INT,
+--     name VARCHAR(50),
+--     location VARCHAR(50),
+--     price_range INT
+-- );
+-- -- making surethatv all entries are imp to enter
+-- CREATE TABLE restaurants(
+--     id BIGSERIAL NOT NULL,
+--     name VARCHAR(50) NOT NULL,
+--     location VARCHAR(50) NOT NULL,
+--     price_range INT NOT NULL
+-- );
+-- -- making id as primary key
+-- CREATE TABLE restaurants(
+--     id BIGSERIAL NOT NULL PRIMARY KEY,
+--     name VARCHAR(50) NOT NULL,
+--     location VARCHAR(50) NOT NULL,
+--     price_range INT NOT NULL
+-- );
