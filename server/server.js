bfe7a467bebacbe6a0fc9d57ea43a7ccf115468e
sejal99 <sejal8974@gmail.com>
2024-03-16 22:24:52 +0530

    server  setup

--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
-const express=require('express')
-const dotenv=require('dotenv')
-const db =require ('./db') 
+const express = require("express");
+const dotenv = require("dotenv");
+const db = require("./db");
 dotenv.config();
-const app=express();
-
+const app = express();
 
 app.use(express.json());
 
-app.get("/api/v1/restaurants",async(req,res)=>{
-    const results=await db.query("select * from restaurants");
+app.get("/api/v1/restaurants", async (req, res) => {
+  try {
+    const results = await db.query("select * from restaurants");
     console.log(results);
     res.status(200).json({
-        status:'success',
-        data:{
-            restaurant:["mcdonalds","wendys"],
-        }
-    })
-})
-const port=process.env.port||3001
-app.listen(port,()=>{
-    console.log('server running on port',port);
+      status: "success",
+      data: restaurants.rows,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+const port = process.env.port || 3001;
+app.listen(port, () => {
+  console.log("server running on port", port);
 });
