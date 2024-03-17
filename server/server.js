78d851794ebcd7ccf94899effef92674c3c0583e
sejal99 <sejal8974@gmail.com>
2024-03-17 17:38:43 +0530

    get all restaurants route

--- server/server.js
+++ server/server.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const db = require("./db");
-dotenv.config();
-const app = express();
+// const express = require("express");
+// const dotenv = require("dotenv");
+// const db = require("./db");
+// dotenv.config();
+// const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get("/api/v1/restaurants", async (req, res) => {
-  try {
-    const results = await db.query("select * from restaurants");
-    console.log('kkkkkkkkkkk',results);
-    res.status(200).json({
-      status: "success",
-      results:results.rows.length,
-      data:{ restaurants:results.rows,}
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
-const port = process.env.port || 3001;
-app.listen(port, () => {
-  console.log("server running on port", port);
-});
+// app.get("/api/v1/restaurants", async (req, res) => {
+//   try {
+//     const results = await db.query("select * from restaurants");
+//     console.log('kkkkkkkkkkk',results);
+//     res.status(200).json({
+//       status: "success",
+//       results:results.rows.length,
+//       data:{ restaurants:results.rows,}
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+// const port = process.env.port || 3001;
+// app.listen(port, () => {
+//   console.log("server running on port", port);
+// });
